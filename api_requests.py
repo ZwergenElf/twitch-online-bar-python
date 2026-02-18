23e5d3b0b111074831931ed3fb0308c170bf8f7d
EinbeinStrasse <bloodpixellp@gmail.com>
2026-02-18 23:33:29 +0100

    refactor: split main logic

---
+++ api_requests.py
@@ -0,0 +1,61 @@
+import sqlite3
+import requests
+import api_requests
+import webbrowser
+
+base_url = "https://api.twitch.tv/helix"
+client_id = "d1knruskmvex6wy0cdq6bu5fy2n5ov"
+
+def get_headers(token):
+    return {
+        "Authorization": f"Bearer {token}",
+        "Client-Id": client_id
+    }
+
+def get_request(endpoint, token):
+    request_url = f"{base_url}/{endpoint}"
+    try:
+        response = requests.get(request_url, headers=get_headers(token))
+        if response.status_code != 401:
+            response.raise_for_status()
+
+    except requests.exceptions.HTTPError as e:
+        print("HTTP error occurred:", e)
+
+    return response
+    
+
+def get_user_id(name, token):
+    response = get_request(f"users?login={name}", token)
+    return response
+    
+def get_followed_id(user_id, token):
+    followed = get_request(f"streams/followed?user_id={user_id}", token)
+    return followed
+
+def post_device_auth():
+    url = "https://id.twitch.tv/oauth2/device"
+    headers = { "Content-Type": "application/x-www-form-urlencoded" }
+    data = f"client_id={client_id}&scopes=user:read:follows"
+    response = requests.post(url, headers=headers, data=data)
+    response_json = response.json()
+    webbrowser.open(response_json['verification_uri'])
+    return response_json['device_code']
+
+def retrieve_tokens(scopes, grant_type = 'urn:ietf:params:oauth:grant-type:device_code'):
+    device_code = post_device_auth()
+    files = {
+    'client_id': (None, client_id),
+    'scopes': (None, scopes),
+    'device_code': (None, device_code),
+    'grant_type': (None, grant_type),
+    }
+
+    response = requests.post('https://id.twitch.tv/oauth2/token', files=files)
+    while response.status_code == 400:
+        response = requests.post('https://id.twitch.tv/oauth2/token', files=files)
+        
+    return response.json()
+
+def get_tokens():
+    return retrieve_tokens(scopes="user:read:follows")
--- main.py
+++ main.py
@@ -1,81 +1,52 @@
 import tkinter as tk
-import requests
-import webbrowser
 import sqlite3
-import time
 
-base_url = "https://api.twitch.tv/helix"
-client_id = "d1knruskmvex6wy0cdq6bu5fy2n5ov"
-def get_headers(token):
-    return {
-        "Authorization": f"Bearer {token}",
-        "Client-Id": client_id
-    }
+from api_requests import get_followed_id, get_tokens, get_user_id, retrieve_tokens
+from web_image import WebImage
 
-def new_request(endpoint, token):
-    request_url = f"{base_url}/{endpoint}"
-    response = requests.get(request_url, headers=get_headers(token))
-    response.raise_for_status()
-    return response.json()["data"]
-
-def get_user_id(name, token):
-    user_id = new_request(f"users?login={name}", token)[0]["id"]
-    return user_id
-    
-def get_followed_id(user_id, token):
-    followed = new_request(f"streams/followed?user_id={user_id}", token)
-    # return filter(lambda streamer: streamer['type'] == "live", followed)
-    return followed
-
-def post_device_auth():
-    url = "https://id.twitch.tv/oauth2/device"
-    headers = { "Content-Type": "application/x-www-form-urlencoded" }
-    data = "client_id=d1knruskmvex6wy0cdq6bu5fy2n5ov&scopes=user:read:follows"
-    response = requests.post(url, headers=headers, data=data)
-    response_json = response.json()
-    webbrowser.open(response_json['verification_uri'])
-    return response_json['device_code']
-
-def retrieve_tokens(client_id, scopes, device_code):
-    files = {
-    'client_id': (None, client_id),
-    'scopes': (None, scopes),
-    'device_code': (None, device_code),
-    'grant_type': (None, 'urn:ietf:params:oauth:grant-type:device_code'),
-    }
-
-    response = requests.post('https://id.twitch.tv/oauth2/token', files=files)
-    while response.status_code == 400:
-        response = requests.post('https://id.twitch.tv/oauth2/token', files=files)
-        
-    return response.json()['access_token']
+user_name = 'pixel_claw'
  
 connection = sqlite3.connect("auth.db")
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS token (id INTEGER PRIMARY KEY, name TEXT, token TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS token (user_name TEXT PRIMARY KEY, access_token TEXT, refresh_token TEXT)")
 
 # Check access token at the start and not at every request, to prevent multiple calls to database
 # Maybe move inside new request later, to get a new access token, if it expires
-cursor.execute("SELECT * FROM token WHERE name = 'access_token'")
-access_token = cursor.fetchone()
+cursor.execute("SELECT * FROM token WHERE user_name = ?", [user_name])
+saved_token = cursor.fetchone()
 
-if access_token is None:
-    device_code = post_device_auth()
-    token = retrieve_tokens(client_id=client_id, scopes="user:read:follows", device_code=device_code)
-    cursor.execute("INSERT INTO token (name, token) VALUES (?, ?)", ('access_token', token))
+if saved_token is None:
+    token = get_tokens()
+    cursor.execute("INSERT INTO token (user_name, access_token, refresh_token) VALUES (?, ?, ?)", [user_name, token['access_token'], token['refresh_token']])
+    access_token = token['access_token']
     connection.commit()
 
 else:
-    token = access_token[2]
+    access_token = saved_token[1]
 
 connection.close()
 
-while True:
-    user_id = get_user_id('pixel_claw', token)
-    followed = get_followed_id(user_id, token)
-    live_streamers = list(map(lambda streamer: { streamer['user_name']}, followed))
-    for streamer in live_streamers:
-        print(streamer)
-    print("<--------------------------------->")
-    # time.sleep(5)
+response = get_user_id(user_name, access_token)
+if response.status_code == 401:
+    connection = sqlite3.connect("auth.db")
+    cursor = connection.cursor()
+    refresh_token = saved_token[2]
+    token = retrieve_tokens(scopes="user:read:follows", grant_type=refresh_token)
+    cursor.execute("UPDATE token SET access_token = ?, refresh_token user_name = ?", [user_name, token['access_token'], token['refresh_token']])
+    connection.close()
+    access_token = token['access_token']
+    response = get_user_id(user_name, access_token)
 
+user_id = response.json()['data'][0]['id']
+
+followed = get_followed_id(user_id, access_token).json()['data']
+print(followed)
+
+url = followed[0]["thumbnail_url"].replace("{width}", "1080").replace("{height}", "720")
+
+root = tk.Tk()
+img = WebImage(url).get()
+image_lab = tk.Label(root, image=img)
+image_lab.grid(row=0, column=0)
+
+root.mainloop()
